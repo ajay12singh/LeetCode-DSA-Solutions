e100030377a613bf87f0c7015afab00b7acabd15
ajay12singh <ajaysinghshekhawat3221@gmail.com>
2026-07-04 16:20:37 +0530

    Time: 15 ms (48.23%), Space: 29.1 MB (35.05%) - LeetHub

---
+++ 0475-heaters/0475-heaters.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findRadius(vector<int>& houses, vector<int>& heaters) {
+
+        sort(houses.begin(),houses.end());
+        sort(heaters.begin(),heaters.end());
+        int ans=0;
+        int current_ans  = 0;
+
+        for ( int i = 0 ; i < houses.size();i++){
+
+            auto it = lower_bound(heaters.begin(), heaters.end(), houses[i]);
+            
+
+            int distRight = (it == heaters.end()) ? INT_MAX : *it - houses[i];
+            int distLeft = (it == heaters.begin()) ? INT_MAX : houses[i] - *prev(it);
+           
+            current_ans = min(distLeft, distRight);
+            ans = max(ans,current_ans);
+        }
+        return ans;
+        
+    }
+};
