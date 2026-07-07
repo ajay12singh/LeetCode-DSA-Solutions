f004842be508e4633e65ea835d0fc48df4296e51
ajay12singh <ajaysinghshekhawat3221@gmail.com>
2026-07-07 15:37:14 +0530

    Time: 1 ms (25.35%), Space: 14.5 MB (14.09%) - LeetHub

--- 0948-bag-of-tokens/0948-bag-of-tokens.cpp
+++ 0948-bag-of-tokens/0948-bag-of-tokens.cpp
@@ -6,26 +6,28 @@ public:
         int p1 = 0;
         int p2 = tokens.size()-1;
         int score=0;
+        int maxscore=0;//save the score if score == 1 and remaining one element at last so not decrement on score 
         while(p1<=p2){
 
             if(tokens[p1]<=power){
                 power -= tokens[p1];
                 score++;
                 p1++;
+                maxscore=max(score,maxscore);
             }
             else{
-            if(score>=1 && p1 != p2){
+            if(score>=1 ){
                     power +=tokens[p2];
                     p2--;
                     score-=1;
                 }
                 else{
-                    p2--;
+                    break;
                 }
             }
         }
          
-         return score;
+         return maxscore;
 
     }
 };
