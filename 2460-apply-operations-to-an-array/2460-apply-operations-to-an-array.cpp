514d9ba1e96b18084a1ed213ebeffa264bf5a144
ajay12singh <ajaysinghshekhawat3221@gmail.com>
2026-07-31 14:16:33 +0530

    Time: 0 ms (100%), Space: 13.3 MB (27.46%) - LeetHub

--- 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.cpp
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.cpp
@@ -18,25 +18,56 @@ public:
             }
         }
      
-    p1 = 0,p2 = 1;
+    // p1 = 0,p2 = 1;
+    // while(p2<nums.size()){
+
+    //     if(nums[p1]==0){
+    //         if(nums[p2]==0){
+    //             p2++;
+    //         }
+    //         else{
+    //             swap(nums[p1],nums[p2]);
+    //             p1++;
+    //             p2++;
+    //         }
+
+    //     }
+    //     else{
+    //         p1++;
+    //         p2++;
+    //     }
+    // }
+
+    p1 = 0;
+    p2 = 0;
     while(p2<nums.size()){
 
-        if(nums[p1]==0){
-            if(nums[p2]==0){
+        if(nums[p2]==0){
+            if(nums[p1] == 0){
                 p2++;
             }
             else{
+                p2++;
+            }
+            
+        }
+        else{
+            if(nums[p1] == 0){
                 swap(nums[p1],nums[p2]);
                 p1++;
                 p2++;
-            }
 
-        }
-        else{
+            }
+           else{
             p1++;
             p2++;
+           }
         }
+        
     }
+    
+
+    
     return nums;
     }
 };
