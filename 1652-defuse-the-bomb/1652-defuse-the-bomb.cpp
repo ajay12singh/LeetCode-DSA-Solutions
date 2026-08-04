46211a4d562ca6a701df60602d145f65b74ae842
ajay12singh <ajaysinghshekhawat3221@gmail.com>
2026-08-04 16:10:25 +0530

    Time: 0 ms (100%), Space: 11.5 MB (84.82%) - LeetHub

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.cpp
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.cpp
@@ -2,48 +2,33 @@ class Solution {
 public:
     vector<int> decrypt(vector<int>& code, int k) {
         
-        vector<int> result;
-        int sum = 0;
-    if(k>0){
-        
-        for(int i = 0; i < k ; i++){
+        int i = -1,j=-1;
+        vector<int> result(code.size(),0);
 
-            sum += code[i];
-
+        if(k == 0) return result;
+        if(k>0){
+            i = 1;
+            j = k; 
         }
 
-        int j = 0;
-        while(j<code.size()){
+        else {
+            i = code.size() - abs(k);
+            j = code.size() - 1;
+        }
+        int window = 0;
+        for(int pointer = i; pointer <=j ; pointer++){
+            window += code[pointer];
+        }
 
-            int x = (j+k)%code.size();
+        for(int k= 0 ; k < code.size(); k++){
 
-            sum += code[x]-code[j];
+            result[k] = window;
+
+            window -= code[i % code.size()];
+            i++;
+            window += code [(j+1)% code.size()];
             j++;
-
-            result.push_back(sum);
-
         }
-    }
-    
-    else if (k < 0) {
-            int abs_k = abs(k);
-            for (int i = 0; i < code.size(); i++) {
-                int sum = 0;
-                for (int j = 1; j <= abs_k; j++) {
-                    int index = (i - j + code.size()) % code.size();
-                    sum += code[index];
-                }
-                result.push_back(sum);
-            }
-        }
-    
-    else {
-        for(int j = 0; j<code.size(); j++){
-            result.push_back(0);
-        }
-    }
-
-      
 
         return result;
     }
