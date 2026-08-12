4eb3f520f117f88117e7ee4a7592d1932d3fab1e
ajay12singh <ajaysinghshekhawat3221@gmail.com>
2026-08-12 06:28:33 +0530

    Time: 56 ms (12.53%), Space: 84.9 MB (34.63%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -4,17 +4,26 @@ public:
         unordered_map<int, int> freq;
         int i = 0, j = 0;
         int ans = 0;
+        int k = 2;
 
         while (j < fruits.size()) {
             freq[fruits[j]]++;
 
-    
-            while (freq.size() > 2) {
+            int size = freq.size();
+            int allow = k - size; 
+           
+            while (allow < 0) {
                 freq[fruits[i]]--;
+                
+               
                 if (freq[fruits[i]] == 0) {
-                    freq.erase(fruits[i]); 
+                    freq.erase(fruits[i]);
                 }
+                
                 i++;
+                
+                
+                allow = k - freq.size(); 
             }
 
             ans = max(ans, j - i + 1);
