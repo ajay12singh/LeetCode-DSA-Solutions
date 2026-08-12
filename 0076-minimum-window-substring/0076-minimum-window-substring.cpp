9323bbe976af8d5874344e47f7248c5594019c07
ajay12singh <ajaysinghshekhawat3221@gmail.com>
2026-08-12 14:54:28 +0530

    Time: 7 ms (61.13%), Space: 11.5 MB (65.19%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        int n = s.length();
+        
+        if (t.length() > n)
+            return "";
+            
+        unordered_map<char, int> mp;
+        
+        // Store frequency of characters of string t in map
+        for (char &ch : t)
+            mp[ch]++;
+            
+        int requiredCount = t.length();
+        int i = 0, j = 0;
+        
+        int minWindowSize = INT_MAX;
+        int start_i = 0;
+        
+        // Story Starts
+        while (j < n) {
+            char ch = s[j];
+            
+            if (mp[ch] > 0)
+                requiredCount--;
+                
+            mp[ch]--;
+            
+            // Try to shrink the window from the left (i) when all characters are found
+            while (requiredCount == 0) {
+                int currWindowSize = j - i + 1;
+                
+                if (currWindowSize < minWindowSize) {
+                    minWindowSize = currWindowSize;
+                    start_i = i;
+                }
+                
+                mp[s[i]]++;
+                
+                if (mp[s[i]] > 0) {
+                    requiredCount++;
+                }
+                
+                i++;
+            }
+            
+            j++;
+        }
+        
+        return minWindowSize == INT_MAX ? "" : s.substr(start_i, minWindowSize);
+    }
+};
