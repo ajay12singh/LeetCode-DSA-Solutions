7cdd580b7af8eed06627e6866c652db13124fd59
ajay12singh <ajaysinghshekhawat3221@gmail.com>
2026-08-14 10:35:07 +0530

    Time: 8 ms (37.48%), Space: 102.3 MB (92.34%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,43 +1,40 @@
-
-
-        class Solution {
+class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
-        int n = nums.size();
-        
-        unordered_map<int, int> mp;
-        int sum = 0;
-        
-      
-        mp[0] = -1;
-        
-       
-        for(int i = 0; i < n; i++) {
-            sum += nums[i];
-            mp[sum] = i; 
+
+        int total_sum=0;
+
+        for(int i = 0; i< nums.size();i++){
+
+            total_sum += nums[i];
+
         }
-        
-        
-        if(sum < x) {
-            return -1;
-        }
-        
-        int remainingSum = sum - x; 
-        int longestSubArray = INT_MIN;
-        
-        sum = 0;
-        for(int i = 0; i < n; i++) {
-            sum += nums[i];
-            
-            int findSum = sum - remainingSum;
-            
-            if(mp.find(findSum) != mp.end()) {
-                int idx = mp[findSum];
-                longestSubArray = max(longestSubArray, i - idx);
+
+        if(total_sum<x) return -1;
+
+        int i = 0; 
+        int j  = 0;
+        int current_sum = 0;
+        int target = total_sum - x;
+        int ans = -1;
+        while(j < nums.size()){
+
+            current_sum += nums[j];
+
+          
+
+
+
+            while(target < current_sum ) {
+                current_sum -= nums[i];
+                i++;
             }
+              if(target == current_sum )  ans = max(ans, j-i+1);
+
+            j++;
         }
         
-        return longestSubArray == INT_MIN ? -1 : n - longestSubArray;
+      if(ans == -1  ) return ans;
+      else return nums.size() - ans;
     }
 };
-   
