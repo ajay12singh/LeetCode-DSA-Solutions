9411eb109dcd63ba99d1aebdb32afcbf76c4c3c2
ajay12singh <ajaysinghshekhawat3221@gmail.com>
2026-08-22 20:10:07 +0530

    Time: 0 ms (100%), Space: 74.1 MB (33.08%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
     vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
-        vector<int> res(n, 0);
+        vector<int> diff(n + 2, 0);
 
-        for (int i= 0 ; i < bookings.size();i++) {
-            // Index 0: first flight, Index 1: last flight, Index 2: seats
-            for (int j = bookings[i][0]; j <= bookings[i][1]; j++) {
-                res[j-1] += bookings[i][2];
-            }
+        for (auto &b : bookings) {
+            int start = b[0];
+            int end = b[1];
+            int count = b[2];
+
+            diff[start] += count;
+            diff[end + 1] -= count;
         }
 
-        return res;
+        // Cumulative sum
+        vector<int> result;
+        int cumSum = 0;
+        for (int i = 1; i <= n; i++) {
+            cumSum += diff[i];
+            result.push_back(cumSum);
+        }
+
+        return result;
     }
 };
