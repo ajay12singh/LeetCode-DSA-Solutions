ad340e8ee50b6e424c998742efe37a4afd0e1493
ajay12singh <ajaysinghshekhawat3221@gmail.com>
2026-08-22 16:40:40 +0530

    Time: 20 ms (79.88%), Space: 88.4 MB (43.16%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.cpp
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -6,15 +6,15 @@ public:
 
         map[0] = -1;
 
-        for(int i = 0; i < nums.size(); i++){
+        // for(int i = 0; i < nums.size(); i++){
 
-            if(nums[i]==0) nums[i] = -1;
-        }
+        //     if(nums[i]==0) nums[i] = -1;
+        // }
         int ans = 0;
         int sum = 0;
         for(int i = 0;  i < nums.size(); i++){
 
-            sum+=nums[i];
+            sum += (nums[i] == 1)? 1:-1;
 
             if(map.find(sum) != map.end()){
                 ans = max(ans,i - map[sum]);
