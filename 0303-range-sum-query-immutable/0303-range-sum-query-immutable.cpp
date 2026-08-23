97d824e8b7d4a744fff2bbb46bb7f19eba3d4ed4
ajay12singh <ajaysinghshekhawat3221@gmail.com>
2026-08-23 19:22:06 +0530

    Time: 0 ms (100%), Space: 23.9 MB (94.79%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
@@ -1,29 +1,18 @@
 class NumArray {
 private:
-    vector<int> prefix1;
-    vector<int> prefix2;
+    vector<int> prefix;
 
 public:
     NumArray(vector<int>& nums) {
         int n = nums.size();
-        prefix1.resize(n);
-        prefix2.resize(n);
-
-        // Your logic: prefix1[i] stores the sum of elements BEFORE index i
-        prefix1[0] = 0;
-        for (int i = 1; i < n; i++) {
-            prefix1[i] = prefix1[i - 1] + nums[i - 1];
-        }
-
-        // Your logic: prefix2[i] stores the cumulative sum UP TO index i
-        prefix2[0] = nums[0];
-        for (int i = 1; i < n; i++) {
-            prefix2[i] = prefix2[i - 1] + nums[i];
+        prefix.resize(n + 1, 0);
+        
+        for (int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] + nums[i];
         }
     }
     
     int sumRange(int left, int right) {
-        // Your logic: total sum up to 'right' minus the sum before 'left'
-        return prefix2[right] - prefix1[left];
+        return prefix[right + 1] - prefix[left];
     }
 };
