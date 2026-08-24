7c44180a67743ff1cff44d5485b73d4a596ae477
ajay12singh <ajaysinghshekhawat3221@gmail.com>
2026-08-24 14:00:09 +0530

    Time: 28 ms (30.56%), Space: 88.4 MB (43.15%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.cpp
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -19,6 +19,7 @@ public:
             if(map.find(sum) != map.end()){
                 ans = max(ans,i - map[sum]);
             }
+            // why else due to we don't want to push sum = 0 again in new index because we want to get max subarray !
             else map[sum] = i;
 
         }
