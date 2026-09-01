2abbe71cc1df64a2004e81f08d35369fe6b5f2f5
ajay12singh <ajaysinghshekhawat3221@gmail.com>
2026-09-01 13:04:10 +0530

    Time: 0 ms (100%), Space: 43.6 MB (47.85%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
@@ -1,47 +1,40 @@
-#include <vector>
-#include <numeric>
-#include <algorithm>
-
 class Solution {
 private:
-
-    int kadaneMax(std::vector<int>& nums) {
+    int kadaneMax(vector<int>& nums) {
         int max_sum = nums[0];
-        int current_sum = nums[0];
-        
-        for (int i = 1; i < nums.size(); ++i) {
-            current_sum = std::max(nums[i], current_sum + nums[i]);
-            max_sum = std::max(max_sum, current_sum);
+        int current_sum = 0;
+
+        for (int x : nums) {
+            current_sum += x;
+            if (current_sum > max_sum) max_sum = current_sum;
+            if (current_sum < 0) current_sum = 0; 
         }
         return max_sum;
     }
 
-    int kadaneMin(std::vector<int>& nums) {
+    int kadaneMin(vector<int>& nums) {
         int min_sum = nums[0];
-        int current_sum = nums[0];
-        
-        for (int i = 1; i < nums.size(); ++i) {
-            current_sum = std::min(nums[i], current_sum + nums[i]);
-            min_sum = std::min(min_sum, current_sum);
+        int current_sum = 0;
+
+        for (int x : nums) {
+            current_sum += x;
+            if (current_sum < min_sum) min_sum = current_sum;
+            if (current_sum > 0) current_sum = 0; 
         }
         return min_sum;
     }
 
 public:
-    int maxSubarraySumCircular(std::vector<int>& nums) {
-     
-        int total_sum = std::accumulate(nums.begin(), nums.end(), 0);
+    int maxSubarraySumCircular(vector<int>& nums) {
+        int total_sum = 0;
+        for (int x : nums) total_sum += x;
 
         int max_kadane = kadaneMax(nums);
         int min_kadane = kadaneMin(nums);
-            
+
+        if (max_kadane < 0) return max_kadane;
+
         int circular_sum = total_sum - min_kadane;
-
-       
-        if (max_kadane < 0) {
-            return max_kadane;
-        }
-
-          return std::max(max_kadane, circular_sum);
+        return max_kadane > circular_sum ? max_kadane : circular_sum;
     }
 };
