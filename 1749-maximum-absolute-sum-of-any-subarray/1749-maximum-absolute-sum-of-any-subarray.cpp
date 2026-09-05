a41b584789f767dd10bdd8e2c664447d1983656f
ajay12singh <ajaysinghshekhawat3221@gmail.com>
2026-09-05 19:21:28 +0530

    Time: 4 ms (9.35%), Space: 45.1 MB (89.45%) - LeetHub

--- 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.cpp
+++ 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.cpp
@@ -2,34 +2,47 @@ class Solution {
 public:
     int maxAbsoluteSum(vector<int>& nums) {
 
-        int min_sum = INT_MAX; 
-        int max_sum = INT_MIN;
-        int current_sum = 0;
+        // int min_sum = INT_MAX; 
+        // int max_sum = INT_MIN;
+        // int current_sum = 0;
         
 
-        for(int i = 0 ; i< nums.size();i++){
+        // for(int i = 0 ; i< nums.size();i++){
 
-            current_sum += nums[i];
+        //     current_sum += nums[i];
 
-            if(current_sum<0) current_sum = 0;
-            max_sum = max(max_sum,current_sum);
+        //     if(current_sum<0) current_sum = 0;
+        //     max_sum = max(max_sum,current_sum);
+        // }
+
+        //   current_sum = 0;
+
+
+        //  for(int i = 0 ; i< nums.size();i++){
+
+        //     current_sum += nums[i];
+
+        //     if(current_sum>0) current_sum = 0;
+        //     min_sum = min(min_sum,current_sum);
+        // }
+
+
+        // return max(abs(min_sum),abs(max_sum));
+
+
+
+       
+        int ps = 0;
+        int mi = 0;
+        int mx = 0;
+        for(int i: nums){
+            ps+=i;
+            if(mi > ps) mi = ps;
+            if(mx < ps) mx = ps;
         }
-
-          current_sum = 0;
-
-
-         for(int i = 0 ; i< nums.size();i++){
-
-            current_sum += nums[i];
-
-            if(current_sum>0) current_sum = 0;
-            min_sum = min(min_sum,current_sum);
-        }
-
-
-        return max(abs(min_sum),abs(max_sum));
-
-
+        if(mx == mi) return abs(mi);
+        return abs(mi-mx);
+    
 
 
         
