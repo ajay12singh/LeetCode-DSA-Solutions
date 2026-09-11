e01017656833db5818e35bf535e5989e29e421bb
ajay12singh <ajaysinghshekhawat3221@gmail.com>
2026-09-11 10:39:44 +0530

    Time: 10 ms (7.78%), Space: 14 MB (13.22%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+       
+        unordered_map<char, int> freqMap;
+        int maxFreq = 0;
+        for (char c : s) {
+            freqMap[c]++;
+            maxFreq = max(maxFreq, freqMap[c]);
+        }
+
+      
+        vector<string> buckets(maxFreq + 1, "");
+        for (auto& [ch, count] : freqMap) {
+            buckets[count].push_back(ch);
+        }
+
+      
+        string res = "";
+        for (int i = maxFreq; i > 0; i--) {
+            for (char c : buckets[i]) {
+                res.append(i, c);
+            }
+        }
+
+        return res;
+    }
+};
