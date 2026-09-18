beb1aaa1c41558e2274c1ba9c7a918873c1f2907
ajay12singh <ajaysinghshekhawat3221@gmail.com>
2026-09-18 17:34:07 +0530

    Time: 0 ms (100%), Space: 10.3 MB (58.21%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,48 +1,28 @@
-
-
 class Solution {
 public:
     string reverseWords(string s) {
-        // Step 1: Clean spaces and reverse whole string
-        int read = 0, write = 0, n = s.length();
+
+
+        reverse(s.begin(),s.end());
+
+        int  i = 0, j = 0, k = 0;
+
+        while(i< s.length()){
+
+            while(i< s.length() && s[i] != ' '){
+                s[k++] = s[i++];
+            }
+
+            if(j<k){
+                reverse(s.begin()+j , s.begin()+k);
+                s[k] = ' ';
+                k++;
+                j=k;
+            }
+
+            i++;
+        }
         
-        while (read < n) {
-            while (read < n && s[read] == ' ') read++; // Skip extra spaces
-            if (read < n) {
-                if (write > 0) s[write++] = ' '; // Insert single space separator
-                while (read < n && s[read] != ' ') {
-                    s[write++] = s[read++];
-                }
-            }
-        }
-        s.resize(write); // Remove trailing space memory
-
-        reverse(s.begin(), s.end()); // Reverse entire cleaned string
-
-        // Step 2: Your word-by-word reversal logic
-        int i = 0, j = 0, k = 0;
-        int len = s.length();
-
-        while (j < len) {
-            // Find the end of the current word
-            while (j < len && s[j] != ' ') {
-                j++;
-            }
-            
-            k = j - 1; // Last character of the current word
-
-            // Reverse the individual word using your two-pointer swap
-            while (i < k) {
-                swap(s[i], s[k]);
-                i++;
-                k--;
-            }
-
-            // Move pointers to start of next word
-            j++; 
-            i = j;
-        }
-
-        return s;
+        return s.substr(0,k-1);
     }
 };
